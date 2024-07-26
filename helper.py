f00281514ea3fa2ffde1491a4807bdf879ab4849
atharva3-c <123581459+atharva3-c@users.noreply.github.com>
2024-07-26 11:06:26 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,11 +1,8 @@
-import urlextract
-from urlextract import URLExtract
+
 from wordcloud import WordCloud
 import pandas as pd
 from collections import Counter
-import emoji
 
-extract = URLExtract()
 
 
 def fetch_stats(selected_user, df):
